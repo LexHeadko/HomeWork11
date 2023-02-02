8ab4931af5d64c1639e0ca8f2c5985e64fe2e8e6
LexHeadko <n-odes11rus@yandex.ru>
2023-02-02 19:32:45 +0200

    Задача 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
+import java.time.LocalDate;
+
 public class Main {
-    public static void year(int temp) {
+    public static void findYear(int temp) {
         int year = temp;
         if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
             ;
@@ -8,9 +10,23 @@ public class Main {
             System.out.println(year + " год не является високосным");
         }
     }
+    public static void mobileApp(int yearRelease, byte osClient) {
+        int yearOfRelease = yearRelease;
+        byte clientOS = osClient; // 0 - iOS, 1 - AndroidOS
+        int currentYear = LocalDate.now().getYear();
+        if (clientOS == 0 && yearOfRelease < currentYear) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (clientOS == 1 && yearOfRelease < currentYear) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else if (clientOS == 0 && yearOfRelease > currentYear) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientOS == 1 && yearOfRelease > currentYear) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+    }
     public static void main(String[] args) {
         task1();
-        //task2();
+        task2();
         //task3();
         //task4();
         //task5();
@@ -23,9 +39,16 @@ public class Main {
     public static void task1() {
         System.out.println("Задача 1");
         int year = 2000;
-        year(year);
+        findYear(year);
     }
     public static void task2() {
         System.out.println("Задача 2");
+        int yearOfRelease = 2015;
+        byte clientOS = 1; // 0 - iOS, 1 - AndroidOS
+        mobileApp(yearOfRelease, clientOS);
+    }
+    public static void task3() {
+        System.out.println("Задача 3");
+
     }
 }
